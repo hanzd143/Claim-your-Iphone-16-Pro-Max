0bbf87c1a9fad6595115bb7b17972acb53e9eca2
hanzd143 <hanz091505@gmail.com>
2025-08-26 20:36:13 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('claimBtn').addEventListener('click', function() {
+  // Cocomelon - Wheels on the Bus (YouTube)
+  // This link opens in YouTube app if available, or browser otherwise
+  window.location.href = 'https://www.youtube.com/watch?v=GzrjwOQpAl0';
+});
